ac295f52995ae18bef7726a9d2c8937106b228d0
Iuri Matias <iuri.matias@gmail.com>
2019-02-02 16:25:17 +0100

    refactor user list

---
+++ src/components/User.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import FiberManualRecord from '@material-ui/icons/FiberManualRecord';
+import FiberManualRecordOutlined from '@material-ui/icons/FiberManualRecordOutlined';
+import Jazzicon, { jsNumberForAddress } from 'react-jazzicon'
+import Tooltip from '@material-ui/core/Tooltip';
+import blueGrey from '@material-ui/core/colors/blueGrey';
+import green from '@material-ui/core/colors/green';
+
+import UserAvatar from './UserAvatar'
+
+const online = green['500'];
+const offline = blueGrey['500'];
+
+const User = ({pubkey, username, lastSeen, isOnline}) => (
+  <ListItem button key={pubkey} style={{ display: 'flex', paddingLeft: '5px' }}>
+    <div style={{ display: 'flex' }}>
+      {isOnline ? <FiberManualRecord style={{ color: online, margin: 'auto' }} /> : <FiberManualRecordOutlined style={{ color: offline, margin: 'auto' }} />}
+      <UserAvatar pubkey={pubkey} />
+    </div>
+    <Tooltip title={`Last seen on ${new Date(lastSeen)}`} placement="top-start">
+      <ListItemText primary={username} />
+    </Tooltip>
+  </ListItem>
+);
+
+export default User;
